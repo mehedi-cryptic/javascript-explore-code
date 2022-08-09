260b56329c0ec28ea987c85fea4517eae5b76a94
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-08-09 23:06:52 +0600

    basic array

---
+++ array.js
@@ -0,0 +1,20 @@
+var friendsAge=[12,11,43,24,34];
+ friendsAge[2]=41;
+console.log(friendsAge);
+var positon=friendsAge.indexOf(141);
+friendsAge.push(10);
+friendsAge.push(35);
+console.log(friendsAge);
+friendsAge.pop();
+console.log(friendsAge);
+
+
+var tealine=['Kalam','Salam','Balam','Malam'];
+var katakati=tealine.slice(1,3);
+console.log(katakati);
+console.log(tealine);
+console.log(tealine);
+tealine.push('Jalam');
+console.log(tealine);
+tealine.unshift('Palam');
+console.log(tealine);
