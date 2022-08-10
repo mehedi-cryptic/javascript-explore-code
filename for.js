a280e13c48e2df8c9f05052641c27781f41c38de
mehedi-cryptic <dexterousmehedi977@gmail.com>
2022-08-11 00:58:47 +0600

    loop and more basics

---
+++ for.js
@@ -0,0 +1,5 @@
+var nums=[22, 33, 24, 45, 86, 43, 32 ];
+for(var i=0; i<nums.length; i++){
+    var element=nums[i];
+    console.log(element);
+}
---
+++ function.js
@@ -0,0 +1,17 @@
+function sayLoveYou(num){
+    var result=num*2;
+    //console.log(result);
+    return result;
+}
+var first=sayLoveYou(5);
+var sec=sayLoveYou(100);
+var total=first+sec;
+console.log(total);
+
+
+function add(num1, num2){
+    var res=num1+num2;
+    return res;
+}
+var tot=add(15,17);
+console.log(tot);
